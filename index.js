b2bbd38cedb15da9f51efd99597d9cd9ce305aeb
kat <katmacdonald1996@gmail.com>
2024-10-22 12:40:16 +0100

    add datetime + kat text to footer

---
+++ index.js
@@ -0,0 +1,7 @@
+// create a new 'Date' object
+const now = new Date();
+
+// get the current date and time as a string
+const currentDateTime = now.toLocaleString();
+
+console.log(currentDateTime); // output: "10/22/2024, 2:28:15 PM" (will vary depending on your time zone)
